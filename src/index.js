eea32c4389a5217b5a6c7dfcc698ec418bc7154d
Jasper van Merle <jaspervmerle@gmail.com>
2020-01-29 23:29:16 +0100

    Initial implementation

--- src/index.js
+++ src/index.js
@@ -1,7 +1,54 @@
+import { remote } from 'electron';
+
+let windowFocusedCallback = null;
+let windowCreatedCallback = null;
+
+let isFocused = false;
+
+function centerWindow(backWindow, frontWindow) {
+  const backBounds = backWindow.getBounds();
+  const backCenterX = backBounds.x + Math.round(backBounds.width / 2);
+  const backCenterY = backBounds.y + Math.round(backBounds.height / 2);
+
+  const frontBounds = frontWindow.getBounds();
+  const frontX = backCenterX - Math.round(frontBounds.width / 2);
+  const frontY = backCenterY - Math.round(frontBounds.height / 2);
+
+  frontWindow.setBounds({
+    ...frontBounds,
+    x: frontX,
+    y: frontY,
+  });
+}
+
+function onWindowFocused(win) {
+  isFocused = win === inkdrop.window;
+}
+
+function onWindowCreated(win) {
+  if (isFocused) {
+    centerWindow(inkdrop.window, win);
+  }
+}
+
 export function activate() {
-  console.log('Plugin activated');
+  windowFocusedCallback = (_, win) => onWindowFocused(win);
+  windowCreatedCallback = (_, win) => onWindowCreated(win);
+
+  remote.app.on('browser-window-focus', windowFocusedCallback);
+  remote.app.on('browser-window-created', windowCreatedCallback);
+
+  isFocused = inkdrop.window.isFocused();
 }
 
 export function deactivate() {
-  console.log('Plugin deactivated');
+  if (windowFocusedCallback !== null) {
+    remote.app.off('browser-window-focus', windowFocusedCallback);
+    windowFocusedCallback = null;
+  }
+
+  if (windowCreatedCallback !== null) {
+    remote.app.off('browser-window-created', windowCreatedCallback);
+    windowCreatedCallback = null;
+  }
 }
